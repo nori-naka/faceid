d766fb46800d4a885a23b16ddbaab17cbca94a42
Nori <noriaki.nakamura@gmail.com>
2023-01-20 11:34:32 +0900

    画面レイアウトの問題を修正

--- src/components/FaceCanvas.vue
+++ src/components/FaceCanvas.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from "vue";
+import { ref, onMounted, onUnmounted, computed } from "vue";
 import * as faceapi from 'face-api.js';
 import InputForm from "./InputForm.vue";
 import ModalPage from "./ModalPage.vue";
@@ -21,26 +21,25 @@ const loadModels = async () => {
   loadingMessage.value = "モデルローディング中"
 
 	return Promise.all([
-  // faceapi.loadSsdMobilenetv1Model(MODEL_URL),
-  faceapi.loadTinyFaceDetectorModel(MODEL_URL),
-  // faceapi.loadMtcnnModel(MODEL_URL),
-  // faceapi.loadFaceLandmarkModel(MODEL_URL),
-  faceapi.loadFaceLandmarkTinyModel(MODEL_URL),
-  faceapi.loadFaceRecognitionModel(MODEL_URL),
-  // faceapi.loadFaceExpressionModel(MODEL_URL),
+    // faceapi.loadSsdMobilenetv1Model(MODEL_URL),
+    faceapi.loadTinyFaceDetectorModel(MODEL_URL),
+    // faceapi.loadMtcnnModel(MODEL_URL),
+    // faceapi.loadFaceLandmarkModel(MODEL_URL),
+    faceapi.loadFaceLandmarkTinyModel(MODEL_URL),
+    faceapi.loadFaceRecognitionModel(MODEL_URL),
+    // faceapi.loadFaceExpressionModel(MODEL_URL),
 	])
 }
 
 const video_start = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
-    video: { width: 360 },
+    video: {width: 640},
     audio: false
   });
   loadingMessage.value = loadingMessage.value + " / video enable"
   videoRef.value.srcObject = stream;
   await videoRef.value.play();
-  canvasRef.value.width = videoRef.value.videoWidth;
-  canvasRef.value.height = videoRef.value.videoHeight;
+
   return stream.value
 }
 
@@ -49,25 +48,30 @@ let faceMatcher = null;
 let ctx = null;
 
 const registPhoto = async (name) => {
-  console.log(name)
+  console.log(name);
+  isReady.value = false;
+
   ctx = canvasRef.value.getContext("2d");
+  canvasRef.value.width = videoRef.value.videoWidth;
+  canvasRef.value.height = videoRef.value.videoHeight;
   ctx.drawImage(videoRef.value, 0, 0, canvasRef.value.width, canvasRef.value.height);
   imgRef.value.src = canvasRef.value.toDataURL();
   // canvasRef.value.style.opacity = 0;
 
   loadingMessage.value = "参照データ作成中"
-  isReady.value = false;
   // まずは参照データ
   detection = await faceapi.detectSingleFace(imgRef.value, new faceapi.TinyFaceDetectorOptions())
     .withFaceLandmarks(true).withFaceDescriptor();
   const labeledDescriptor = new faceapi.LabeledFaceDescriptors(name, [detection.descriptor])
   faceMatcher = new faceapi.FaceMatcher(labeledDescriptor, 0.6);
+  console.log("参照データ完了")
   isReady.value = true;
 }
 
 let clearId = null;
 onMounted(async() => {
   await loadModels();
+  console.log("loadModels ended...");
   await video_start();
   isReady.value = true;
   const cnt = ref(0);
@@ -80,6 +84,8 @@ onMounted(async() => {
 
       cnt.value = 0;
       ctx.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height);
+      canvasRef.value.style.width = `${videoRef.value.clientWidth}px`;
+      canvasRef.value.style.height = `${videoRef.value.clientHieght}px`;
       detections.forEach(fd => {
         const bestMatch = faceMatcher.findBestMatch(fd.descriptor);
         console.log(bestMatch.toString());
@@ -102,21 +108,29 @@ onUnmounted(() => {
 
 </script>
 <template>
-  <div class="control" style="background: #eee; margin: 100px;">
-    <InputForm v-model="registName" @onSubmit="registPhoto"></InputForm>
-    <div class="column is-12">
-      <div style="position: relative">
-        <video ref="videoRef"></video>
-        <canvas ref="canvasRef" class="canvasClass"></canvas>
+  <div class="content">
+    <div class="container is-max-desktop">
+      <h1 class="title">顔認識</h1>
+      <p class="subtitle">名前を入力して「登録」を押してください</p>
+      <InputForm v-model="registName" @onSubmit="registPhoto" @onEnter="registPhoto"></InputForm>
+      <div class="columns is-vcentered">
+        <div class="column is-half">
+          <div style="position: relative">
+            <video ref="videoRef"></video>
+            <canvas ref="canvasRef" class="is-overlay"></canvas>
+          </div>
+        </div>
+        <div class="column is-half">
+          <img class="column" ref="imgRef"/>
+        </div>
       </div>
-      <img ref="imgRef"/>
     </div>
   </div>
   <ModalPage :show="!isReady" :msg="loadingMessage"></ModalPage>
 </template>
 <style>
 
-.canvasClass {
+.absoluteClass {
   position: absolute;
   top: 0px;
   left: 0px;
--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,18 +1,23 @@
 <script setup>
 import { ref } from "vue";
 const props = defineProps(["modelValue"]);
-const emits = defineEmits(["update:modelValue", "onSubmit"]);
+const emits = defineEmits(["update:modelValue", "onSubmit", "onEnter"]);
 
 const registName = ref("")
 
 const onCahnge = (e) => { emits("update:modelValue", registName.value) }
 const onSubmit = (e) => { emits("onSubmit", registName.value) }
+const onEnter = (e) => {
+  if (e.key === "Enter") {
+    emits("onEnter", registName.value);
+  }
+}
 
 </script>
 <template>
   <div class="field has-addons">
     <div class="control">
-      <input class="input" type="text" placeholder="登録者名" v-model="registName" @change="onCahnge">
+      <input class="input" type="text" placeholder="登録者名" v-model="registName" @change="onCahnge" @keydown.enter="onEnter">
     </div>
     <div class="control">
       <a class="button is-success" @click="onSubmit">登録</a>
